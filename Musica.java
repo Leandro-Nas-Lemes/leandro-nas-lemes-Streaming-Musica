abcac7ae373c8d745df41f89f76178347df5f53f
Leandro do Nascimento Lemes <leandronaslemes@gmail.com>
2026-03-27 19:20:57 -0300

    Adicionando arquivos do projeto

---
+++ Musica.java
@@ -0,0 +1,29 @@
+public class Musica {
+
+    String titulo;
+    String artista;
+    int duracaoSegundos;
+    String genero;
+
+    void exibir() {
+        System.out.printf("- %s | %s | %s | %s%n",
+                titulo,
+                artista,
+                getDuracaoFormatada(),
+                genero);
+    }
+
+    String getDuracaoFormatada() {
+        int minutos = duracaoSegundos / 60;
+        int segundos = duracaoSegundos % 60;
+        return String.format("%02d:%02d", minutos, segundos);
+    }
+
+    boolean contemTitulo(String busca) {
+        return titulo.toLowerCase().contains(busca.toLowerCase());
+    }
+
+    boolean contemArtista(String busca) {
+        return artista.toLowerCase().contains(busca.toLowerCase());
+    }
+}
